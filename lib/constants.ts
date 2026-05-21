87c8e47883689b71800c3de2fcd1237e4034941d
TI Carbo <ti@grupocarbo.com.br>
2026-05-21 10:45:57 -0300

    feat: update CHECKOUT_URL to Vindi checkout (CarboZé Moto 10ml)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const CHECKOUT_URL = "#"; // Substituir pelo link real de checkout
+export const CHECKOUT_URL = "https://app.vindi.com.br/customer/pages/702a5924-c5d5-4084-964c-9d68aaf7f291/subscriptions/new";
 
 export const WHATSAPP_NUMBER = "5511400021128"; // 11 4000-2112
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
